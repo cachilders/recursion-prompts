cfa84e083316afe174b60f3f40ce13fab5335ef2
Casey Childers <cachilders@gmail.com>
2018-01-30 22:15:58 -0800

    solve ten more plus a divide partial

--- src/recursion.js
+++ src/recursion.js
@@ -105,16 +105,47 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (x === 0 && y === 0) return NaN;
+  if (x === 0) return 0;
+  if (x < 0) {
+    if (y < 0) {
+    if (x - y > 0) return x;
+      return modulo(x - y, y);
+    }
+    if (x + y > 0) return x;
+    return modulo(x + y, y);
+  }
+  if (y < 0) {
+    if (x + y < 0) return x;
+    return modulo(x + y, y);
+  }
+  if (x - y < 0) return x;
+  return modulo(x - y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (y === 0 || x === 0) return 0;
+  if (y < 0 && x < 0) {
+    x = x - x - x;
+    y = y - y - y;
+  } else if (x < 0) {
+    const temp = x;
+    x = y;
+    y = temp;
+  }
+  if (y < 0) return -x + multiply(x, y + 1)
+  return x + multiply(x, y - 1);
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods.
 var divide = function(x, y) {
+  if (x === 0) return 0;
+  if (y === 0) return NaN;
+  if (x < 0) return 0;
+  return 1 + divide(x - y, y);
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
@@ -123,6 +154,15 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x < 0) return null;
+  if (y < 0) return null;
+  if (y > x) {
+    let temp = y;
+    y = x;
+    x = temp;
+  }
+  if (x - y === 0) return x;
+  return gcd(x - y, y);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
@@ -130,21 +170,29 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) return true;
+  return str1[0] === str2[0] && compareStr(str1.slice(1), str2.slice(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  if (str.length === 0) return [];
+  return [str[0]].concat(createArray(str.slice(1)))
 };
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  if (array.length === 0) return []
+  return [array[array.length - 1]].concat(reverseArr(array.slice(0, -1)))
 };
 
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function(value, length) {
+  if (length === 0) return [];
+  return [value].concat(buildList(value, length - 1));
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
@@ -153,17 +201,33 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+  let x;
+  if (n === 0) return []
+  if (n % 3 === 0 && n % 5 === 0) {
+    x = ['FizzBuzz'];
+  } else if (n % 3 === 0) {
+    x = ['Fizz'];
+  } else if (n % 5 === 0) {
+    x = ['Buzz'];
+  } else {
+    x = [n.toString()];
+  }
+  return fizzBuzz(n - 1).concat(x);
 };
 
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  if (array.length === 0) return 0;
+  return (array[0] === value ? 1 : 0) + countOccurrence(array.slice(1), value);
 };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+  if (array.length === 0) return [];
+  return [callback(array[0])].concat(rMap(array.slice(1), callback));
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
